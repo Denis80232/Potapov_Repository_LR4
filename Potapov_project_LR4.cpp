5668b6ac9f4c46f0afa547de01fd881cce4b7574
Denis80232 <dp754594@gmail.com>
2025-03-24 15:59:12 +0300

    Create void main

---
+++ Potapov_project_LR4.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+void Task1(){
+    //developing...
+}
+void Task2(){
+    //developing...
+}
+
+void Task3(){
+    //developing...
+}
+
+void Task4(){
+    //developing...
+}
+
+int main(){
+    cout << "Меню: " << endl;
+    cout << "Ввод вещественного числа x." << endl;
+    cout << "Ввод вещественного числа y." << endl;
+    cout << "Нахождение целой части разности этих 2-х чисел." << endl;
+    cout << "Нахождение дробной части разности этих 2-х чисел." << endl;
+    
+}
